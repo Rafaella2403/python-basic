79dbd21a91b53686c4f4f69117ef925225ebe8c3
rafaella.britto <rafaella.britto@midway.com.br>
2024-10-12 18:01:40 -0300

    exercício 2

---
+++ exercicios/ex_002.py
@@ -0,0 +1,9 @@
+nome = 'Rafaella Ribeiro'
+altura = 1.75
+peso = 92.5
+
+imc = peso / (altura * altura)
+
+print(f'{nome} tem {altura} de altura')
+print(f'pesa {peso} quilos e seu IMC é')
+print(f'{imc:.2f}')
