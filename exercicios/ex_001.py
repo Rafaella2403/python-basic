3672548993d7639052e6069ba0ff88e3858a895c
rafaella.britto <rafaella.britto@midway.com.br>
2024-10-12 18:01:34 -0300

    exercício 1

---
+++ exercicios/ex_001.py
@@ -0,0 +1,17 @@
+# Imprima os valores do nome, sobrenome, idade, ano de nascimento, se é maior de idade e a altura em metros.
+
+nome = 'Rafaella'
+sobrenome = 'Ribeiro Lima Britto'
+full_name = nome+ ' ' + sobrenome
+idade = 26
+ano_nascimento = 1998
+maior_de_idade = idade > 18
+altura_metros = 1.75
+
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Nome completo:', full_name)
+print('Idade:', idade)
+print('Ano de nascimento:', ano_nascimento)
+print('É maior de idade?', maior_de_idade)
+print('Altura em metros:', altura_metros)
