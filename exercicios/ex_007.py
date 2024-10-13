af41865b9d4b04ea137c2014dd8b0775b4f39fc1
rafaella.britto <rafaella.britto@midway.com.br>
2024-10-13 10:25:58 -0300

    exercício 7

---
+++ exercicios/ex_007.py
@@ -0,0 +1,20 @@
+"""
+Faça um programa que peça o primeiro nome do usuário.
+Se o nome tiver 4 letras ou menos escreva
+  "Seu nome é curto"
+Se tiver entre 5 e 6 letras, escreva
+  "Seu nome é normal"
+Maior que 6 escreva
+  "Seu nome é muito grande"
+"""
+
+nome = input("Digite seu primeiro nome: ")
+
+tamanho_nome = len(nome)
+
+if tamanho_nome <= 4:
+    print("Seu nome é curto")
+elif 5 <= tamanho_nome <= 6:
+    print("Seu nome é normal")
+else:
+    print("Seu nome é muito grande")
